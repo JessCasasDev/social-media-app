cd4b5e33085b03eed9ab984fd09399e0e6c28ce7
Jess Github <jlcasasg@unal.edu.co>
2020-03-10 12:06:29 -0500

    feat: user_login: Add login api rest

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -147,6 +147,41 @@ app.post('/signup', (request, response) => {
             }
             return response.status(500).json({ error: error.code });
         });
+});
+
+
+app.post('/login', (request, response) => {
+    const user = {
+        email: request.body.email,
+        password: request.body.password
+    }
+
+    let errors = {}
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if (Object.keys(errors) > 0) {
+        return response.status(400).json(errors);
+    }
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return response.json({ token })
+        })
+        .catch(error => {
+            console.error(error);
+            if (error.code === "auth/wrong-password") {
+                //Unauthorized!
+                return response.status(403).json({ general: 'Wrong credential. Please try again' });
+            }
+
+            return response.status(500).json({ error: error.code });
+        })
 })
+
+
 
 exports.api = functions.https.onRequest(app);
