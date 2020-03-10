b58435772f044bc58aa1ae95e497ff96c6cd5dfc
Jess Github <jlcasasg@unal.edu.co>
2020-03-10 16:33:26 -0500

    feat: user_details: Add api rest to get user details

--- dbschema.js
+++ dbschema.js
@@ -7,5 +7,40 @@ let db = {
             likeCount: 5,
             commentCount: 2,
         }
+    ],
+    users: [
+        {
+            userId: 'SomeIdrequiried',
+            email: 'user_email@yopmail.com',
+            handle: 'user',
+            createdAt: "2020-03-10T15:33:38.414Z",
+            imageUrl: 'some/path/to/add/image',
+            bio: 'Bio for user',
+            website: 'https://no-user.com',
+            location: 'Bogota, Colombia',
+        }
+    ]
+}
+
+const userDetails = {
+    credentials: {
+        userId: 'SomeIdrequiried',
+        email: 'user_email@yopmail.com',
+        handle: 'user',
+        createdAt: "2020-03-10T15:33:38.414Z",
+        imageUrl: 'some/path/to/add/image',
+        bio: 'Bio for user',
+        website: 'https://no-user.com',
+        location: 'Bogota, Colombia',
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'sdfsodfidf',
+        },
+        {
+            userHandle: 'user',
+            screamId: 'QWEcDfS345SSS',
+        }
     ]
 }
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,6 +1,6 @@
 
 const isEmpty = (string) => {
-    return string && string.trim() === '';
+    return string.trim() === '';
 }
 
 const isEmail = (email) => {
@@ -38,5 +38,20 @@ exports.validateLoginData = (data) => {
         errors,
         valid: Object.keys(errors).length === 0
     }
+}
 
+exports.reduceUserDetails = (data) => {
+    let userDetails = {};
+
+    if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+    if (!isEmpty(data.website.trim())) {
+        if (data.website.trim().substring(0, 4) !== 'http') {
+            userDetails.website = 'https://' + data.website.trim();
+        } else {
+            userDetails.website = data.website;
+        }
+    }
+    if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+    return userDetails;
 }
