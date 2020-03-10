823144c5b737d00e399b1ed55068bcbe7bfbafb5
Jess Github <jlcasasg@unal.edu.co>
2020-03-10 14:43:50 -0500

    refactor: index.js split functionalities, utils, into ordered folders

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAsCt5s63GiDxrAA3Af-bDQzFRqe8daD5g",
+    authDomain: "social-media-e5acf.firebaseapp.com",
+    databaseURL: "https://social-media-e5acf.firebaseio.com",
+    projectId: "social-media-e5acf",
+    storageBucket: "social-media-e5acf.appspot.com",
+    messagingSenderId: "272151115782",
+    appId: "1:272151115782:web:7beabde055e0062bcbeb87",
+    measurementId: "G-1W0TXZXH8D"
+};
