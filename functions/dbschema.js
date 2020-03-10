0f0a6a6abcf0161289d56df6c3d77f099f3f0033
Jess Github <jlcasasg@unal.edu.co>
2020-03-10 10:39:12 -0500

    Database: add database schema and update request methods post/get

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'usesr',
+            body: 'body',
+            createdAt: "2020-03-10T15:33:38.414Z",
+            likeCount: 5,
+            commentCount: 2,
+        }
+    ]
+}
