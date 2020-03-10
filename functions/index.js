b24b231f2d6ca8c94dd4569736604af0d8372ce5
Jess Github <jlcasasg@unal.edu.co>
2020-03-10 16:33:26 -0500

    feat: user_details: Add api rest to get user details

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,16 @@ const functions = require('firebase-functions');
 
 const app = require('express')();
 const { getAllScreams, postScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 const FBAuth = require('./util/fbAuth')
 
 //Users routes
 app.post('/signup', signup);
-app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+
 
 //Screams Routes
 app.get('/screams', getAllScreams);
