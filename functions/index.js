08a31ea8669bb1ef76d3780905259c3265b52c32
Jess Github <jlcasasg@unal.edu.co>
2020-03-11 08:50:05 -0500

    feat: scream_comment. Get comments on Post Detail

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 const functions = require('firebase-functions');
 
 const app = require('express')();
-const { getAllScreams, postScream } = require('./handlers/screams');
+const { getAllScreams, postScream, getScream } = require('./handlers/screams');
 const { signup, login, uploadImage } = require('./handlers/users');
 const FBAuth = require('./util/fbAuth')
 
@@ -14,7 +14,7 @@ app.post('/user/image', FBAuth, uploadImage)
 //Screams Routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postScream);
-
+app.get('/scream/:screamId', getScream)
 
 
 exports.api = functions.https.onRequest(app);
